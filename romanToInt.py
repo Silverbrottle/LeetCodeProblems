99e7866802890366ebb15ce2a8a60f4cf99bd03e
Abhinav Pisharody <40502339+Silverbrottle@users.noreply.github.com>
2024-11-26 00:19:55 +0530

    13. Roman to Integer

---
+++ romanToInt.py
@@ -0,0 +1,19 @@
+def romanToInt(str):
+        length=len(str)
+        chr={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        rev_str=[str[length-1-i] for i in range(0,length)]
+        sum1=0
+        prev_val=0
+        for i in range(0,length):
+            val_from_chr=int(chr[rev_str[i]])
+            if(val_from_chr<prev_val):
+                sum1=sum1-val_from_chr
+            else:
+                sum1=sum1+val_from_chr
+            prev_val=val_from_chr
+
+
+        return sum1
+        
+
+print(romanToInt("LVIII"))
